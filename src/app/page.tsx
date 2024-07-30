912ca5a39709b7b2620e5d612a74553bfe672eff
Alex Nascimento <alexjose68@hotmail.com>
2024-07-30 12:43:53 -0300

    feat: em Home criei uma nova função assíncrona chamada getGamesData para fazer uma nova chamada a nossa api buscando uma rota que trás todos os itens/jogos da api, transformei em json e criei uma const data que espera esta função getGamesData resolver e passeia tipagem para ela como um array de da nossa interface GameProps. Tambpem criei um novo componente chamado GameCard, passei uma propriedade chamada data para ele e tipei com uma interface chamada GameCardPRops, esta interface nada mais é que uma propriedade data que é do tipo GameProps. Logo, data tem acesso as prorpriedades que setamos em Gameprops e por tanto ao que queremos pegar da nossa api para preencher cada card representado pelo nosso componente GameCard. Estilizei o componente e passei as propriedade que queremos apresentar no mesmo. Então importamos em Home e criamos um map para percorrer o array da const data e preencher cada GameCard com um item receberndo o que temos em cada item percorrido o que passamos na propriedade data que criamos no componente GameCard.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { BsArrowRightSquare } from "react-icons/bs";
 import { Input } from "../components/input"
+import { GameCard } from "@/components/GameCard";
 
 async function getDalyGame() {
   try {
@@ -16,9 +17,20 @@ async function getDalyGame() {
   }
 }
 
+// nova chamada, para listar todos os jogos:
+async function getGamesData() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=games`, {next: { revalidate: 320 }});
+    return res.json();
+  } catch(err) {
+    throw new Error("Failed to fetch data");
+  }
+}
+
 export default async function Home() {
 
   const dalyGame: GameProps = await getDalyGame();
+  const data: GameProps[] = await getGamesData();
 
   return (
     <main className="w-full">
@@ -45,7 +57,17 @@ export default async function Home() {
             </div>
           </section>
         </Link>
+
         <Input />
+
+        <h2 className="text-lg font-bold mt-8 mb-5">
+          Jogos para conhecer
+        </h2>
+        <section className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {data.map( (item) => (
+            <GameCard key={item.id} data={item} />
+          ))}
+        </section>
       </Container>
     </main>
   );
