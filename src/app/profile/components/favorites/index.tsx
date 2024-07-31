6cf8d7068155ad5f2591094a193fbb8377440438
Alex Nascimento <alexjose68@hotmail.com>
2024-07-31 20:52:14 -0300

    feat: Transformei meu componente FavoriteCard de server component para client component para poder fazer mudança de estados com usestate, e criei funcionalidades para o input, e renderizações condicionais para que o usuário possa ecrever e guardar nomes de seus jogos favoritos!

--- src/app/profile/components/favorites/index.tsx
+++ src/app/profile/components/favorites/index.tsx
@@ -1,13 +1,55 @@
+"use client"
+import { useState } from 'react';
 import { FiEdit, FiX } from 'react-icons/fi';
 
 export function FavoriteCard() {
-  return (
-    <div className="w-full bg-gray-800 p-4 h-44 text-white rounded-lg flex justify-between flex-col">
-      <button className='self-start hover:scale-110 duration-300 transition-all'>
-        <FiEdit size={24} color='#FFF' />
-      </button>
+  const [input, setInput] = useState("");
+  const [showInput, setShowInput] = useState(false);
+  const [gameName, setGameName] = useState("");
 
-      <p className='font-bold text-white'>Adicionar jogo</p>
+  function handleButton() {
+    setShowInput(!showInput);
+
+    if (input !== "") {
+      setGameName(input);
+    }
+
+    setInput("");
+  }
+
+  return (
+    <div className="w-full bg-gray-900 p-4 h-44 text-white rounded-lg flex justify-between flex-col">
+      { showInput ? (
+        <div className='flex items-center justify-center gap-3'>
+          <input
+            className='w-full rounded-md h-8 text-black px-2'
+            type="text"
+            value={input}
+            onChange={ (e) => setInput(e.target.value) }
+          />
+          <button onClick={handleButton}>
+            <FiX size={24} color='#FFF' />
+          </button> 
+        </div>
+      ) : (
+        <button
+          className='self-start hover:scale-110 duration-300 transition-all'
+          onClick={handleButton}
+        >
+          <FiEdit size={24} color='#FFF' />
+        </button>
+      )}
+
+      {gameName && (
+        <div>
+          <span className='text-white'>Jogo Favorito:</span>
+          <p className='font-bold text-white'>{gameName}</p>
+        </div>
+      )}
+
+      {!gameName && (
+        <p className='font-bold text-white'>Adicionar jogo</p>
+      )}
     </div>
   )
 }
